12b8723715056e59cf944854c47677afe2b69665
ShadowyBlue <alandchung09@gmail.com>
2024-03-18 18:45:44 -0500

    enabled GH actions

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ export default function Banner() {
     return (
         <>
         <header>Orbit Report</header>
-        <p>Click on the buttons to see the satellites in that orbit type</p>
+        <p>Click on the buttons to see the satellites in that orbit type.</p>
         </>
     );
 }
